bc5b282dca22872507c68c710b80176d9d2c31de
[sushantskd1357] <[sushantdutta.pro1357@gmail.com]>
2020-09-26 20:52:19 +0530

    Init commit

---
+++ routes/order.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { updateStock } = require("../controllers/product");
+
+const { getOrderById, createOrder, getAllOrders, getOrderStatus, updateStatus } = require("../controllers/order");
+
+//params
+router.param("/userId", getUserById);
+router.param("/orderId", getOrderById);
+
+//Actual routes
+//create route
+router.post("/order/create/:userId", 
+    isSignedIn,
+    isAuthenticated,
+    pushOrderInPurchaseList,
+    updateStock,
+    createOrder
+);
+
+//read route
+router.get("/order/all/:userId", isSignedIn, isAuthenticated, isAdmin, getAllOrders);
+
+//status
+router.get("/order/status/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    getOrderStatus
+);
+
+router.put("/order/:orderId/:userId", 
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    updateStatus
+);
+
+module.exports = router;
